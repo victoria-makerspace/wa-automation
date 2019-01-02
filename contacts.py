6fe294efb68669daa0cacc3e9985613c26be3af4
Victor van Poppelen <vvanpo@gmail.com>
2019-01-01 21:49:53 -0800

    Abstract contacts

---
+++ contacts.py
@@ -0,0 +1,40 @@
+class Contacts:
+    def __init__(self, session):
+        self.session = session
+        self.response = session.get('contacts')['Contacts']
+        self.levels = {}
+
+        for contact in self.response:
+            contact = Contact(contact)
+
+            if contact.archived:
+                continue
+
+            if contact.level not in self.levels:
+                self.levels[contact.level] = {}
+
+            self.levels[contact.level][contact.ID] = contact
+
+    def get(self, ID):
+        for level in self.levels:
+            if ID in level:
+                return self.levels[level][ID]
+
+        return None
+
+class Contact:
+    def __init__(self, response):
+        self.response = response
+        self.level = None
+        self.ID = response['Id']
+        self.first_name = response['FirstName']
+        self.last_name = response['LastName']
+        self.fields = {}
+
+        if 'MembershipLevel' in response:
+            self.level = response['MembershipLevel']['Name']
+
+        for field in response['FieldValues']:
+            self.fields[field['FieldName']] = field['Value']
+
+        self.archived = self.fields['Archived']
