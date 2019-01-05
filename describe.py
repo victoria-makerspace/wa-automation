ec943b017dd5189ed772143af3f6632bab77250c
Victor van Poppelen <vvanpo@gmail.com>
2019-01-04 22:56:11 -0800

    Add describe command

---
+++ describe.py
@@ -0,0 +1,14 @@
+import sys
+
+def describe(contacts, args):
+    if len(args) is not 1:
+        print('Error: invalid number of arguments to `describe`: requires 1', file = sys.stderr)
+        return
+
+    contact = contacts.find(args[0])
+
+    if contact is None:
+        print(f'Error: e-mail <{args[0]}> not associated with a contact', file = sys.stderr)
+        return
+
+    print(contact.describe())
