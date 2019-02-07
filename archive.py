e56b1c22ed97c5f4e173b78cb63125ee7c60c690
Victor van Poppelen <vvanpo@gmail.com>
2019-02-06 21:14:39 -0800

    Add comments describing archive logic

--- archive.py
+++ archive.py
@@ -2,18 +2,29 @@ from config import config
 from datetime import datetime, timezone
 
 def archive(contacts, now):
+    """
+    Archives all non-members after a configurable number of days.
+    Additionally, specific membership levels may also be configured to be
+    archived.
+    """
     level_msg = ''
 
+    # The 'levels' configuration property is a list of membership levels that we
+    # auto-archive in addition to all non-members.
     if len(config['archive']['levels']):
+        # Format list of membership levels as a comma-delineated string for
+        # readability.
         level_msg = ', '.join([f'"{level}"' for level in config['archive']['levels']])
         level_msg = f'and members in the {level_msg} levels '
 
     archive_msg = f"Archiving non-members {level_msg}who have not logged in within {config['archive']['threshold'].days} days:"
 
     for contact in contacts.getlevel(None, *config['archive']['levels']):
+        # Ignore contacts that are already archived or have never logged in.
         if contact.archived or contact.last_login is None:
             continue
 
+        # Ignore contacts that have logged in recently.
         if now - config['archive']['threshold'] <= contact.last_login:
             continue
 
