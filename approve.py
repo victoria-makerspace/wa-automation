9612ea88deb916f627415cbb531df628ed9a96ed
bisley <kylebisley@gmail.com>
2019-12-18 15:32:46 -0800

    Test of savedsearch endpoint

---
+++ approve.py
@@ -0,0 +1,52 @@
+# (member ID's are going to have to be our identifier for endpoint work)
+# Maybe no? https://gethelp.wildapricot.com/en/articles/502#filtering
+
+# get sublist of members to auto-approve
+#   - run search (appears to have to be a saved search)
+#     - returns list of member ID's
+#   '/accounts/{accountId}/savedsearches/{savedSearchId}'
+# OR----------------------------------------------------
+# search in contacts for contacts fitting discription
+#     member status is Pending - New
+#     Current ballance :NOT: Balance overdue Overpaid
+
+from config import config
+from session import Session
+from datetime import datetime, timezone
+#import pdb 
+
+# for each member ID
+#   approve members applications
+#     - post approve
+#       '/rpc/{accountId}/ApprovePendingMembership'
+
+def getSavedSearchList(session):
+  savedSearchList = session.request('GET', 'savedsearches')
+  print(savedSearchList)
+  return savedSearchList
+
+
+def getSavedSearchIdByName(session, savedSearchList, targetName):
+  for search in savedSearchList:
+    print(search)
+    if search["Name"].lower() == targetName:
+      return search["Id"]
+  return None
+
+# def getListToApprove(session, searchId):
+#   contactsIdList = session.request('GET', 'AprovePendingMembership', searchId)
+  
+#   # for contactId in contactsIdList:
+#   #   response = session.request('POST', /rpc/AprovePendingMembership', contactId)
+#   #   if response != 200:
+#   #       do something with the response
+
+def autoApprove(contacts):
+  session = contacts.session
+  savedSearchList = getSavedSearchList(session)
+  searchId = getSavedSearchIdByName(session, savedSearchList, "list-of-members-to-approve")
+  print("search Results")
+  print(searchId)
+  temp = 'savedSearchList/'+str(searchId)
+  contactIdsToApprove = session.request('GET', 'savedSearchList')
+  print(contactIdsToApprove)
