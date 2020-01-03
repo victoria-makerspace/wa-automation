1f8ba877eec34a654acfde536784a281e5fd9368
bisley <kylebisley@gmail.com>
2020-01-02 22:49:54 -0800

    Parts of the email
    These are parts of the email as outlined in swagger hub

---
+++ email.py
@@ -0,0 +1,10 @@
+
+RECIPIENT ARRAY
+will need Id of security's contact card in our data base
+and copy to system email
+SUBJECT
+subject of email (email filter will collect these in our WA account)
+  BODY
+  will want some sort of short preamble
+  list of members who's cards should be canceled
+
