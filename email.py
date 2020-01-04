cf35f139b69d8f071a3f82ffd0668a9d84dc2455
bisley <kylebisley@gmail.com>
2020-01-04 14:22:45 -0800

    Linter improvments

--- email.py
+++ email.py
@@ -1,50 +1,44 @@
-
-
-
-{
-  "Subject": "string",
-  "Body": "string",
-  "Recipients": [
-    {
-      "Id": 0,
-      "Type": "IndividualRecipient",
-      "Name": "string",
-      "Email": "string"
-    }
-  ],
-  "EventId": 0
-}
+# {
+#   "Subject": "string",
+#   "Body": "string",
+#   "Recipients": [
+#     {
+#       "Id": 0,
+#       "Type": "IndividualRecipient",
+#       "Name": "string",
+#       "Email": "string"
+#     }
+#   ],
+#   "EventId": 0
+# }
 class Email:
-  def __init__(self, subject, body, recipients):
-    self.subject = subject
-    self.body = body
-    self.recipients = recipients
-    self.eventId = 0
-  
-@property
-def emailAddress(self):
-  for email in self.recipients['Email']:
-    emails = emails + ', ' + email
-  return emails
+    def __init__(self, subject, body, recipients):
+        self.subject = subject
+        self.body = body
+        self.recipients = recipients
+        self.event_id = 0
 
-@property
-def subject(self):
-  return self.subject
+    @property
+    def email_address(self):
+        for email in self.recipients['Email']:
+            emails = emails + ', ' + email
+        return emails
 
-@property
-def body(self):
-  return self.body
+    @property
+    def subject(self):
+        return self.subject
 
-@body.setter
-#setter to force all additions to email to be proceeded by a new line
-def body(self, toAppend):
-  self.body = self.body + '\n' + toAppend
+    @property
+    def body(self):
+        return self.body
 
-@property
-def eventId(self):
-  if eventId is not 0:
-    return self.eventId
-  else:
-    return "Email not associated with event"
+    @body.setter
+    #setter to force all additions to email to be proceeded by a new line
+    def body(self, to_append):
+        self.body = self.body + '\n' + to_append
 
-
+    @property
+    def event_id(self):
+        if event_id is not 0:
+            return self.event_id
+        return "Email not associated with event"
--- emailRecipient.py
+++ emailRecipient.py
@@ -1,46 +1,26 @@
 
-Enum:
-[ ]
-Name	string
-Display name of recipient or name of saved search.
+class EmailRecipient:
+    def __init__(self, id, name, email_address, recipient_type=1):
+        # id refers to individual recipient or saved search identifier
+        self.id = id
+        # interger refering to recipient group type (assuming for now that integers are as follows)
+        #   0 IndividualRecipient, 1 EventAttendees_All, 2 EventAttendees_Selected, 3 Contacts_All,
+        #   4 Contacts_Selected, 5 Contacts_SavedSearch, 6 Members_All, 7 Members_SavedSearch,
+        #   8 SentEmailRecipient, 9 EventWaitlist_All, 10 EventWaitlist_Selected
+        self.type = recipient_type
+        # name of recipient or name of saved search.
+        self.name = name
+        self.email = email.set_email(self, recipient_type, email_address)
 
-Email	string
-recipient email. for IndividualRecipient and SentEmailRecipient types only
+    @property
+    def email_address(self):
+        return self.email
 
-}
-
-class SentEmailRecipient:
-  def __init__(self, id, name, emailAddress, recipientType = 1):
-    # id refers to individual recipient or saved search identifier
-    self.Id = id
-
-    # interger refering to recipient group type (assuming for now that integers are as follows)
-    #   1 IndividualRecipient, 
-    #   2 EventAttendees_All, 
-    #   3 EventAttendees_Selected,
-    #   4 Contacts_All,
-    #   5 Contacts_Selected,
-    #   6 Contacts_SavedSearch,
-    #   7 Members_All, 
-    #   8 Members_SavedSearch, 
-    #   9 SentEmailRecipient, 
-    #   10 EventWaitlist_All, 
-    #   11 EventWaitlist_Selected 
-    self.type = recipientType
-
-    # name of recipient or name of saved search.
-    self.name = name
-
-    self.email = email.setter(self, recipientType, emailAddress)
-
-  @property
-  def emailAddress(self):
-    return self.email
-  
-  #setter for emailAddress because it can only be set when type is IndividualRecipient or SentEmailRecipient
-  @email.setter
-  def emailAddress(self, recipientType, email):
-    if recipientType is not 1 or 8:
-      self.email = email
-    else:
-      self.email = None
+    #setter for emailAddress because it can only be set when type is IndividualRecipient
+    # or SentEmailRecipient
+    @email.setter
+    def set_email(self, recipient_type, email_address):
+        if recipient_type is not 1 or 8:
+            self.email = email_address
+        else:
+            self.email = None
