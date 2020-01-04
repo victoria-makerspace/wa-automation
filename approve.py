dd98fa78eaf74daefcb9f50401d001d7e4830750
bisley <kylebisley@gmail.com>
2020-01-03 22:03:25 -0800

    Fixed Linting Errors

--- approve.py
+++ approve.py
@@ -1,35 +1,38 @@
 from config import config
-from session import Session
-from datetime import datetime, timezone
 
-def getSavedSearchList(session):
-  savedSearchList = session.request('GET', 'savedsearches')
-  return savedSearchList
+
+def get_saved_search_list(session):
+    return session.request('GET', 'savedsearches')
 
 # return search id from given target name
-def getSavedSearchIdByName(session, savedSearchList, targetName):
-  for search in savedSearchList:
-    if search["Name"].lower() == targetName:
-      return search["Id"]
-  return None
+
+
+def get_search_id(saved_search_list, target_name):
+    for search in saved_search_list:
+        if search["Name"].lower() == target_name:
+            return search["Id"]
+    return None
 
 # method to approve contact by Id
-def approveMembership(session, contactId):
-  temp = 'ApprovePendingMembership?contactId='+str(contactId)
-  response = session.RPCrequest('POST', temp)
 
-def autoApprove(session):
-  # Search for "list-of-members-to-approve" in the list of all saved searches
-  savedSearchList = getSavedSearchList(session)
-  searchId = getSavedSearchIdByName(session, savedSearchList, config['auto-approve']['searchName'])
-  
-  # Creat a temporary string for search endpoint
-  temp = 'savedsearches/'+str(searchId)
-  searchResults = session.request('GET', temp)
-  
-  # for each contact returned within saved search call the approve method
-  if searchResults["ContactIds"]:
-    contactIdsToApprove = searchResults["ContactIds"]
-    for contact in contactIdsToApprove:
-      approveMembership(session, contact)
-  
+
+def approve_membership(session, contact_id):
+    temp = 'ApprovePendingMembership?contactId='+str(contact_id)
+    session.RPCrequest('POST', temp)
+
+
+def auto_approve(session):
+    # Search for "list-of-members-to-approve" in the list of all saved searches
+    saved_search_list = get_saved_search_list(session)
+    search_id = get_search_id(
+        saved_search_list, config['auto-approve']['searchName'])
+
+    # Creat a temporary string for search endpoint
+    temp = 'savedsearches/'+str(search_id)
+    search_results = session.request('GET', temp)
+
+    # for each contact returned within saved search call the approve method
+    if search_results["ContactIds"]:
+        contact_ids_to_approve = search_results["ContactIds"]
+        for contact in contact_ids_to_approve:
+            approve_membership(session, contact)
