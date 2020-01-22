4a04c196bb00e127893fadc135996824a76956a0
Kyle Bisley <kylebisley@gmail.com>
2020-01-21 20:31:49 -0800

    Timeout working

--- electronic_mail.py
+++ electronic_mail.py
@@ -7,7 +7,6 @@ class electronic_mail:
 
     def convert_to_dic(self):
     # A function takes in a custom object and returns a dictionary representation of the object.
-  
     # Create the dictionary object 
         obj_dict = {}
   
--- timeout.py
+++ timeout.py
@@ -1,65 +1,58 @@
 from config import config
 from contacts import Contacts
-import contact
+from contact import Contact
+from electronic_mail import electronic_mail
+from email_recipient import email_recipient
 from pprint import pprint
 import pdb
 from datetime import datetime
+from approve import approve_membership
+import json
 
-    
+
 def timeout(session, contacts, now):
     
     balance_due = contacts.balance_due()
-    print('balance_due')
-    pprint(balance_due)
 
     for id in balance_due:
         temp = f'invoices?contactId={id}'
         invoice = session.request('GET', temp)
-        print()
         datetime_object = datetime.strptime(invoice['Invoices'][0]['DocumentDate'], '%Y-%m-%dT%H:%M:%S%z')
-        print(contacts.get(id).name)
-
-        if ((now - datetime_object) <= config['application_stale']['timeout']):
-            print(f"Don't archive this person {id}" )
-        else:
-            print(f"Archive this person {id}")
-            with open('./emails/cancel.txt', 'r') as draft:
-                email_body = draft.read()
-            recipient = email_recipient(id, contacts.get(id).name, 0)
             
-    print()
+        if(((now - datetime_object) >= config['application_stale']['timeout']) & 
+                (invoice['Invoices'][0]['OrderType']=='MembershipApplication')):
+            # email timeout draft
+            contact = contacts.get(id)
+            
+            with open('./emails/timeout.txt', 'r') as draft:
+                email_body = draft.read()
+            email_body.format(contact.name)
+            recipient = email_recipient(id, contact, 0)
+            
+            mail = electronic_mail('Membership invoice timed out', email_body, [recipient])
+            j_mail = json.dumps(mail, default=electronic_mail.convert_to_dic)
+            
+            session.request('POST', 'email/SendEmail', rpc=True, data=json.loads(j_mail))
+            # void the invoice
+      
+            invoice_id = int(invoice['Invoices'][0]['Id'])
+            temp = f'VoidInvoice?invoiceId={invoice_id}'
+            invoice = session.request('POST', temp, rpc=True)
 
+            # # cancel their application
+            temp= f'RejectPendingMembership?contactId={id}'
+            session.request('POST', temp, rpc=True)
+            
+            #assign membership level to New Applicant
+            contact = contacts.get(id)
+            data = {'Id': id,
+                    'MembershipLevel': {
+                        'Id': 1113397,
+                        'Name': 'New Applicant'},
+                    'MembershipEnabled': True}
+            temp = f'contacts/{id}'  
+            response = session.request('PUT', temp, data=data)
 
-    #fred blogs
-    # print('fred blogs')
-    # contact_id = str(53438595)
-    # temp = f'invoices?contactId={contact_id}'
-    # pprint(temp)
-    # invoices = session.request('GET', temp)
-    # pprint(invoices)
-    # print('DocumentDate')
-    #pdb.set_trace()
-    # print(type(invoices['Invoices'][0]['DocumentDate']))
-    #ValueError: time data '2019-12-01T13:36:53+00:00' does not match format '%Y %b %d %H:%M:%S:%z''
-    # datetime_object = datetime.strptime(invoices['Invoices'][0]['DocumentDate'], '%Y-%m-%dT%H:%M:%S%z')
-    
-    
-    # print('datetime')
-    # print(type(datetime_object))
-    # print(datetime_object)
-    # print('now-datetime_object')
-    # print(now-datetime_object)
-    # if (now - config['archive']['threshold'] <= datetime_object):
-    #     print('')
-        #send email
-        #set level to new applicant
-        #set contact to archived 
-    # print('')
-    #kyle bisleys
-    # print('kyle bisley')
-    # contact_id = str(53359541)
-    # temp = f'invoices?contactId={contact_id}'
-    # pprint(temp)
-    # invoices = session.request('GET', temp)
-    # pprint(invoices)
-
+            #approve New Applicant Level and Archive the contact
+            approve_membership(session, id)
+            contact.archived = True
